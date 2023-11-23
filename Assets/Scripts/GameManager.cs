2c6310f47595420cff393e5678da7818ad0fa528
kanuengnit-BB <kanuengnit.mali@bumail.net>
2023-11-23 16:07:31 +0700

    shootball

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject cueBall;
     [SerializeField] private GameObject ballLine;
     [SerializeField] private float xInput;
+    [SerializeField] private float force;
     
     // Start is called before the first frame update
     void Start()
@@ -34,6 +35,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         RotateBall();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBall();
+        }
     }
 
     void SetBalls(BallColors color, int pos)
@@ -47,5 +52,12 @@ public class GameManager : MonoBehaviour
     {
         xInput = Input.GetAxis("Horizontal");
         cueBall.transform.Rotate(new Vector3(0f,xInput/10,0f));
+    }
+
+    void ShootBall()
+    {
+        Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+        rd.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
+        ballLine.SetActive(false);
     }
 }
